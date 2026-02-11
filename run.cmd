a2681471b4de3412251c04647b899c0f8d3d08fb
spiritxyz541 <133633070+spiritxyz541@users.noreply.github.com>
2026-02-11 16:38:57 +0700

    Fix project structure for Vercel

--- run.cmd
+++
@@ -1,7 +0,0 @@
-echo "# SuperStore" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/spiritxyz541/SuperStore.git
-git push -u origin main
